e5b05741aabf8403f6f243e0dd11f20992c700d9
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-20 16:13:53 +0530

    ...

--- This/script.js
+++ This/script.js
@@ -16,6 +16,37 @@ show();
 let obj = {
     name :"Harsh",
     sayName:function(){      //Ek aisa function jo object ke andhar ho                         use hum methoda bolte hai
-        console.log(this);
+        console.log(this.name);
     }
 }
+
+//So far we have seen that [this] is Window in Global and Function Scope
+//But in case of Object it is the object itself
+
+//Event Handler
+document.querySelector("h1").addEventListener("click", function() {
+    this.style.color = "red";
+    console.log(this.style.color);
+});
+//this depends on how the handler is defined
+
+//In [class] this->blank object
+
+//In obj method this->object right but there is some exception
+let obj2 = {
+    name: "Harsh",
+    sayName: () => {      //Arrow function due to which this is not working as expected 
+        console.log(this.name);
+    }
+}
+
+//this keyword
+// global - window
+// function - window
+// method with es5 function - Object
+// method with es6 arrow function - window
+// es5 function inside es5 method - window
+// arrow function inside es5 method - Object
+// event handler - Element
+// class - blank object
+
