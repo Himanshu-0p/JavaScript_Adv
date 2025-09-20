bbf0049881915b1230ec6a178141ac64d10ed1cd
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-20 22:59:28 +0530

    ..

--- 5.Promises/script.js
+++ 5.Promises/script.js
@@ -23,45 +23,70 @@
 
 
 //Sync function promise
-const getPromise = () => {
-    return new Promise((resolve,reject)=>{
-        console.log("I am a promise");
-        resolve("success");
-    });
-}
+// const getPromise = () => {
+//     return new Promise((resolve,reject)=>{
+//         console.log("I am a promise");
+//         resolve("success");
+//     });
+// }
 
-let promise = getPromise();
-promise.then(() => {
-    console.log("promise fulfilled");
-})   
+// let promise = getPromise();
+// promise.then(() => {
+//     console.log("promise fulfilled");
+// })   
 
 //Async function promise
-function asyncFunction1(){
+// function asyncFunction1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Some data1");
+//             resolve("success1");
+//         }, 4000);
+//     });
+// }
+
+// function asyncFunction2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Some data2");
+//             resolve("success2");
+//         }, 4000);
+//     });
+// }
+
+// console.log("fetching data1");
+// let p1 = asyncFunction1();
+// p1.then((res) => {
+//     console.log(res);   
+// });
+
+// console.log("fetching data2");
+// let p2 = asyncFunction2();
+// p2.then((res) => {   // ✅ changed from p1 → p2
+//     console.log(res);   
+// });
+
+
+//Promise Chain
+function getData(dataId,getNextData){
     return new Promise((resolve,reject) =>{
         setTimeout(() => {
-            console.log("Some data1");
-            resolve("success");
-        },4000)
+            console.log("data",dataId);
+            resolve("Success");
+            if(getNextData){
+                getNextData();
+            }
+        },5000);
     })
 }
 
-function asyncFunction2(){
-    return new Promise((resolve,reject) =>{
-        setTimeout(() => {
-            console.log("Some data2");
-            resolve("success");
-        },4000)
-    })
-}
-
-console.log("fetching data1");
-let p1 = asyncFunction1();
-p1.then((res) => {
-    console.log(res);   
-});
-
-console.log("fetching data2");
-let p2 = asyncFunction2();
-p1.then((res) => {
-    console.log(res);   
-});
+getData(1)
+   .then((res)=>{
+    return getData(2);
+   })
+   .then((res)=>{
+    return getData(3);
+   })
+   .then((res)=>{
+    console.log(res);
+   });
