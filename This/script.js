518ff577e6caa048086a57e922191fd5c4780a1e
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-20 11:27:50 +0530

    ...

---
+++ This/script.js
@@ -0,0 +1,4 @@
+//This : is a keyword and it is very special
+//Unlike other keywords its key value and nature changes based on the context
+//It is used to refer to the current object
+//It is a reference variable that refers to the current object
