d88525c2a2ecb44e5393cb06358b460e9fedba5d
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-20 11:37:27 +0530

    adding some lines of code

--- This/script.js
+++ This/script.js
@@ -2,3 +2,20 @@
 //Unlike other keywords its key value and nature changes based on the context
 //It is used to refer to the current object
 //It is a reference variable that refers to the current object
+
+
+//global scope
+console.log(this);
+
+//function ke andar
+function show() {
+    console.log(this);
+}
+show();
+
+let obj = {
+    name :"Harsh",
+    sayName:function(){      //Ek aisa function jo object ke andhar ho                         use hum methoda bolte hai
+        console.log(this);
+    }
+}
